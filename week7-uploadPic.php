68a1e9ce5d11642d8ac277ea0a1d0295a79fd1c5
buyphlukthayyaratn-png <buyphlukthayyaratn@gmail.com>
2026-07-07 12:45:56 +0700

    Add files via upload

---
+++ week7-uploadPic.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" enctype="multipart/form-data" action="week7-showPic.php">
+    <input type="file" name="avatar">
+    <input type="submit" value="Upload">
+    </form>
+</body>
+</html>
